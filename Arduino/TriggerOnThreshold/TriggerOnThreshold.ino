e05940c3b4956e8e3d8aeba0111de284d103dc68
Collin Hansen <collinhansen2@gmail.com>
2019-02-13 09:05:06 -0500

    update the code to use more defines instead of hardcoded values
    TEST THIS FIRST

--- Arduino/TriggerOnThreshold/TriggerOnThreshold.ino
+++ Arduino/TriggerOnThreshold/TriggerOnThreshold.ino
@@ -7,7 +7,12 @@
 #define CLK  5  //  Arduino pin 5 connect to HX711 CLK
 #define LED 12  //  Arduino Pin 12 connect to negative on LED with resistor
                 //  Pin 12 -> resistor -> led -> ground
+
+//The default measurements are done at 10Hz
+#define INITIAL_SAMPLES 100 //Number of initial readings to average
+#define LOOP_SAMPLES 5 //Number of readings to average every loop
 #define THRESHOLD 1.3 //Reading/InitialReading
+
 HX711 scale(DOUT, CLK);  // Init of library
 float initialReading; //Used for relative measurement
 void setup() {
@@ -15,13 +20,13 @@ void setup() {
   scale.set_scale(1);  // Start scale
   scale.tare();       // Reset scale to zero
   digitalWrite(LED, HIGH);
-  initialReading = scale.get_units(100);
+  initialReading = scale.get_units(INITIAL_SAMPLES);
   digitalWrite(LED, LOW);
 }
 
 void loop() {
   //Absolute Measurement float known_weight = 2000; //Weight of the thing you are measuring
-  float current_reading=scale.get_units(5);  // get average of 5 scale readings
+  float current_reading=scale.get_units(LOOP_SAMPLES);  // get average of 5 scale readings
   float current_scale=current_reading/initialReading;
   //Absolute Measurement float scale_factor=(current_weight/known_weight);  // divide the result by a known weight
   Serial.print("Raw Reading: ");  // Print the scale factor to use
